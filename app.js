db508ee455545691901b8381e76537b96390c398
Sousafer <fernando.ffsousa@gmail.com>
2022-05-15 20:16:28 +0000

    domain generator

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+
+
+pronoun.forEach(element1 => 
+  adj.forEach(element2 =>
+   noun.forEach(element3 => console.log(element1+element2+element3+".com"))))
+   console.log("Any string");
